408b65a3daac9590e0fcb780e6e5b77f8022cc28
Yue Wu <yue.wu@yale.edu>
2025-02-05 13:07:46 -0500

    we don't need high precision for pL > 0.1 or so

--- src/qec_lego_bench/hpc/submitter/precision_submitter.py
+++ src/qec_lego_bench/hpc/submitter/precision_submitter.py
@@ -13,6 +13,11 @@ class PrecisionSubmitter:
     # minimum number of shots to submit
     min_shots: int = 100
 
+    # also check if the logical error rate is too high (pL > high_pL_threshold)
+    high_pL_threshold: float = 0.1
+    # if so, we don't have to reach that precision because such points are meaningless anyway
+    high_pL_precision: float = 0.2
+
     def __call__(
         self, jobs: Iterable[MonteCarloJob]
     ) -> list[tuple[MonteCarloJob, int]]:
@@ -25,7 +30,10 @@ class PrecisionSubmitter:
             errors = job.result.errors  # type: ignore
             if errors < precision_to_errors(self.min_precision):
                 continue
-            target_errors = precision_to_errors(self.target_precision)
+            target_precision = self.target_precision
+            if errors / job.shots > self.high_pL_threshold:
+                target_precision = self.high_pL_precision
+            target_errors = precision_to_errors(target_precision)
             target_shots = math.ceil(target_errors / errors * job.shots)
             if target_shots < job.expecting_shots:
                 continue
