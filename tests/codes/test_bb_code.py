3695a2d2d021bcc2be24d1e18aaec67d1688ba60
Yue Wu <yue.wu@yale.edu>
2025-02-12 12:42:48 -0500

    add result of bb code

--- tests/codes/test_bb_code.py
+++ tests/codes/test_bb_code.py
@@ -1,24 +1,6 @@
-from qec_lego_bench.codes.stabilizer_codes.css_codes.bb_code import BBCode
+from qec_lego_bench.codes.stabilizer_codes.css_codes.bb_code import bb_code_cli
 
 
 def test_bb_code_construction():
-    code = BBCode()
-
-
-import stim
-
-tableau = stim.Tableau.from_conjugated_generators(
-    xs=[
-        stim.PauliString("+YZ__"),
-        stim.PauliString("-Y_XY"),
-        stim.PauliString("+___Y"),
-        stim.PauliString("+YZX_"),
-    ],
-    zs=[
-        stim.PauliString("+XZYY"),
-        stim.PauliString("-XYX_"),
-        stim.PauliString("-ZXXZ"),
-        stim.PauliString("+XXZ_"),
-    ],
-)
-print(tableau)
+    code = bb_code_cli(n=72, k=12, d=6)
+    print(code)
