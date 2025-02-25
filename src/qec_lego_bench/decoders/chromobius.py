9c0841e004bb6f8ab3db05bc78414e8e6a926210
Yue Wu <yue.wu@yale.edu>
2025-02-24 23:49:45 -0500

    remove chromobius decoder from required dependencies

--- src/qec_lego_bench/decoders/chromobius.py
+++ src/qec_lego_bench/decoders/chromobius.py
@@ -1,7 +1,12 @@
 from qec_lego_bench.cli.decoders import decoder_cli
-import chromobius
 
 
-@decoder_cli("Chromobius", "cb")
-def chromobius_decoder():
-    return chromobius.sinter_decoders()["chromobius"]
+try:
+    import chromobius
+
+    @decoder_cli("Chromobius", "cb")
+    def chromobius_decoder():
+        return chromobius.sinter_decoders()["chromobius"]
+
+except ImportError:
+    pass
